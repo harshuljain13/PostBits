e3ed464a3fb328fbe491347b7b76fc988c36275e
harshul1610 <harshulrobo@gmail.com>
2016-06-27 23:06:01 +0530

    lot of fixes

--- src/main.py
+++ src/main.py
@@ -6,31 +6,49 @@ from firebasepy import FirebaseGet,FirebasePost,FirebasePut,FirebaseDelete
 def main():
 
     MasterJson = FirebaseGet()
-    JSONKeys = MasterJson.keys()
-    for JSONKey in JSONKeys:
-        OneJSON = MasterJson[JSONKey]
-        TextMessage = OneJSON['TextMessage']
-        ImagePath = OneJSON['ImageURL']
-        LinkPath = OneJSON['Link']
-        GroupIds = OneJSON['Groups']
-        Days = OneJSON['Days']
-        Name = OneJSON['Name']
-        #print TextMessage
-        #print ImagePath
-        #print LinkPath
-        #print GroupIds
-        #print Days
-        #print Name
+    """
+    Check if Master json is present. This means whether there is any data to bbe processed or not.
+    """
+    if MasterJson:
+        #assigning keys list to JSONKeys
+        JSONKeys = MasterJson.keys()
 
-        #fbwallpost(perm_Acc_token_fb, PageName, TextMessage, ImagePath, LinkPath)
-        if Days>0:
-            grouppost(usr_access_token, GroupIds, TextMessage, ImagePath, LinkPath, Days,Name)
-            #decrement Days
-            Days-=1
-            OneJSON['Days'] = Days
-            FirebasePut(OneJSON, JSONKey)
-        if Days == 0:
-            FirebaseDelete(JSONKey)
+        for JSONKey in JSONKeys:
+
+            #get JSON for every key
+            OneJSON = MasterJson[JSONKey]
+
+            #get values in this JSON
+            TextMessage = OneJSON['TextMessage']
+            ImagePath = OneJSON['ImageURL']
+            LinkPath = OneJSON['Link']
+            Days = OneJSON['Days']
+            Name = OneJSON['Name']
+
+            #if any groups in selected then process else delete the JSON
+            if 'Groups' in  OneJSON:
+                GroupIds = OneJSON['Groups']
+                #print TextMessage
+                #print ImagePath
+                #print LinkPath
+                #print GroupIds
+                #print Days
+                #print Name
+
+                #fbwallpost(perm_Acc_token_fb, PageName, TextMessage, ImagePath, LinkPath)
+
+                """checking the Days. if 0 then delete the JSON else publish it."""
+                if Days>0:
+                    #grouppost(usr_access_token, GroupIds, TextMessage, ImagePath, LinkPath, Days,Name)
+                    #decrement Days
+
+                    Days-=1
+                    OneJSON['Days'] = Days
+                    FirebasePut(OneJSON, JSONKey)
+                if Days == 0:
+                    FirebaseDelete(JSONKey)
+            else:
+                FirebaseDelete(JSONKey)
 
 if __name__ == '__main__':
     main()
